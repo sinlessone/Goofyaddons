3cec6eb2e68f1aa4f72b8c778f50dcdb32953ffb
adem yahia <mohthegoat1@proton.me>
2026-07-22 17:23:17 +0100

    oops I was looking at a pre 26.1 guide

--- src/client/java/com/goofy/goofyaddons/keybinds/GoofyKeybinds.java
+++ src/client/java/com/goofy/goofyaddons/keybinds/GoofyKeybinds.java
@@ -1,26 +1,29 @@
 package com.goofy.goofyaddons.keybinds;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 
 public class GoofyKeybinds {
 
-    private static final String CATEGORY = "key.categories.goofyaddons";
+    private static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+            ResourceLocation.fromNamespaceAndPath("goofyaddons", "category")
+    );
 
     public static KeyMapping startKey;
     public static KeyMapping stopKey;
 
     public static void register() {
-        startKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        startKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
                 "key.goofyaddons.start",
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_J,
                 CATEGORY
         ));
 
-        stopKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        stopKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
                 "key.goofyaddons.stop",
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_K,
